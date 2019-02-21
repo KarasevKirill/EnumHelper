e7c3f2a065b86dfa5ee43466d34c72af4d6859ea
Kirill Karasev <karasev.kirill.a@gmail.com>
2019-02-21 20:26:08 +1000

    Расширение функционала: поиск значения по строке, возврат дефолтного значения

--- EnumHelper/Helper.cs
+++ EnumHelper/Helper.cs
@@ -2,9 +2,26 @@
 
 namespace EnumHelper
 {
+    /// <summary>
+    /// Позволяет получать строку из аттрибута [Description()] Enum
+    /// или получить значение Enum по значению Description
+    /// </summary>
     public class Helper
     {
-         private T GetAttribute<T>(Enum value) where T : Attribute
+        /// <summary>
+        /// Указывает, необходимо ли вернуть 0, если значение перечисление
+        /// не было найдено при поиске по строке
+        /// </summary>
+        public bool ReturnDefaultValue { get; set; }
+
+        /// <summary>
+        /// Извлекает аттрибут из значения Enum, если аттрибут отсутствует, то
+        /// вернет null
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        private T GetAttribute<T>(Enum value) where T : Attribute
         {
             var memberInfo = value.GetType().GetMember(value.ToString());
 
@@ -13,11 +30,56 @@ namespace EnumHelper
             return attributes.Length > 0 ? (T)attributes[0] : null;
         }
 
-        public string GetDescription(Enum value)
+        /// <summary>
+        /// Возвращает строковое представление значения Enum
+        /// Если имеется аттрибут Description, то вернется его значение
+        /// Если аттрибут отсутствует, то вернется строка, содержащая имя значения
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        public string GetDisplayValue(Enum value)
         {
             var attribute = GetAttribute<DescriptionAttribute>(value);
 
-            return attribute == null ? "" : attribute.Description;
+            return attribute == null ? value.ToString() : attribute.Description;
+        }
+
+        /// <summary>
+        /// Возвращает значение Enum, у которого значение аттрибута Description совпадает с
+        /// переданной строкой поиска.
+        /// Если значение аттрибута не найдено, то возвращает значение, имя которого совпадает
+        /// со строкой поиска.
+        /// Если совпадений нет, то либо вернется 0, либо бросается ArgumentException, в
+        /// зависимости от свойства ReturnDefaultValue
+        /// </summary>
+        /// <typeparam name="E">Enum, по значения которого осуществляется поиск</typeparam>
+        /// <param name="description">Значение аттрибута Description, по которому ведется поиск значения Enum</param>
+        /// <returns></returns>
+        public E GetValueBySearchString<E>(string searchString) where E : struct, IConvertible
+        {
+            if (!typeof(E).IsEnum)
+                throw new ArgumentException("Этому методу для работы необходим Enum");
+
+            foreach (var currentValue in Enum.GetValues(typeof(E)))
+            {
+                string currentDescription = GetDisplayValue((Enum)currentValue);
+
+                if (currentDescription == searchString)
+                    return (E)currentValue;
+            }
+
+            foreach (var currentValue in Enum.GetValues(typeof(E)))
+            {
+                string valueName = currentValue.ToString();
+
+                if (valueName == searchString)
+                    return (E)currentValue;
+            }
+
+            if (ReturnDefaultValue)
+                return default(E);
+
+            throw new ArgumentException("Ни одно значение перечисления не имеет аттрибут Description или имя, совпадающие с искомой строкой");
         }
     }
 }
--- EnumsAttribute/Program.cs
+++ EnumsAttribute/Program.cs
@@ -9,20 +9,56 @@ namespace EnumsAttribute
         {
             var helper = new Helper();
 
-            var first = helper.GetDescription(DocType.FirstValue);
-            var second = helper.GetDescription(DocType.SecondValue);
-            var third = helper.GetDescription(DocType.ThirdValue);
+            Console.WriteLine("Строковое представление значений перечисления, (у значений установлен аттрибут Description):");
+            Console.WriteLine(helper.GetDisplayValue(DocType.FirstValue));
+            Console.WriteLine(helper.GetDisplayValue(DocType.SecondValue));
+            Console.WriteLine(helper.GetDisplayValue(DocType.ThirdValue));
+            Console.WriteLine();
 
-            Console.WriteLine(first);
-            Console.WriteLine(second);
-            Console.WriteLine(third);
+            Console.WriteLine("Строковое представление значения перечисления, (значение не имеет аттрибута):");
+            Console.WriteLine(helper.GetDisplayValue(DocType.FourthValue));
+            Console.WriteLine();
+
+            var firstSearch = "Второе значение";
+            var secondSearch = "ThirdValue";
+            var thirdSearch = "AAAAA";
+
+            Console.WriteLine($"Значение перечисления (поиск по аттрибуту значения, строка поиска: \"{firstSearch}\"):");           
+            Console.WriteLine(helper.GetValueBySearchString<DocType>(firstSearch));
+            Console.WriteLine();
+
+            Console.WriteLine($"Значение перечисления (поиск по имени значения, строка поиска: \"{secondSearch}\"):");
+            Console.WriteLine(helper.GetValueBySearchString<DocType>(secondSearch));
+            Console.WriteLine();
+           
+            Console.WriteLine($"Ищем несуществующее значение, ReturnDefaultValue == true (строка поиска: \"{thirdSearch}\"):");
+            helper.ReturnDefaultValue = true;
+            Console.WriteLine(helper.GetValueBySearchString<DocType>(thirdSearch));
+            Console.WriteLine();
+
+            try
+            {
+                Console.WriteLine($"Ищем несуществующее значение, ReturnDefaultValue == false (строка поиска: \"{thirdSearch}\"):");
+                helper.ReturnDefaultValue = false;
+                Console.WriteLine(helper.GetValueBySearchString<DocType>(thirdSearch));
+                Console.WriteLine();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine("Exception message:");
+                Console.WriteLine(e.Message);
+            }
 
             Console.ReadKey();
         }
     }
-
     public enum DocType : byte
     {
+        /// <summary>
+        /// Ноль считается как не инициализированное значение
+        /// </summary>
+        UndefinedValue = 0,
+
         [Description("Первое значение")]
         FirstValue,
 
@@ -30,6 +66,8 @@ namespace EnumsAttribute
         SecondValue,
 
         [Description("Третье значение")]
-        ThirdValue
+        ThirdValue,
+
+        FourthValue
     }
 }
